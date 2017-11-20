f4a14063a9fcf4c7b8fe1411970bc22569610300
DeMontrius Simon <DeMontriusSimon@gmail.com>
2017-11-19 20:52:31 -0500

    3.1 Tile's Dictionary
    Now a dictionary for all of the tiles' point is added.

--- Tower Defense Tutorial/Assets/Scripts/LevelManager.cs
+++ Tower Defense Tutorial/Assets/Scripts/LevelManager.cs
@@ -11,6 +11,8 @@ public class LevelManager : MonoBehaviour
 	[SerializeField]
 	private CameraMovement cameraMovement;
 
+	public Dictionary<Point,TileScript> Tiles { get; set; }	
+
 	public float TileSize
 	{
 		get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
@@ -29,6 +31,8 @@ public class LevelManager : MonoBehaviour
 
 	private void CreateLevel()
 	{
+		Tiles = new Dictionary<Point, TileScript>();
+
 		string[] mapData = ReadLevelText();
 
 		int mapX = mapData[0].ToCharArray().Length;
@@ -43,18 +47,19 @@ public class LevelManager : MonoBehaviour
 
 			for (int x = 0; x < mapX; x++)
 			{
-				maxTile = PlaceTile(newTiles[x].ToString(), x, y, worldStart);
+				PlaceTile(newTiles[x].ToString(), x, y, worldStart);
 			}
 		}
+		maxTile = Tiles[new Point(mapX - 1, mapY - 1)].transform.position;
 		cameraMovement.SetLimits(new Vector3(maxTile.x + TileSize, maxTile.y - TileSize));
 	}
 
-	private Vector3 PlaceTile(string tileType, int x, int y, Vector3 worldStart)
+	private void PlaceTile(string tileType, int x, int y, Vector3 worldStart)
 	{
 		int tileIndex = int.Parse(tileType);
 		TileScript newTile = Instantiate(tilePrefabs[tileIndex]).GetComponent<TileScript>();
 		newTile.Setup(new Point(x, y), new Vector3(worldStart.x + TileSize * x, worldStart.y - TileSize * y, 0));
-		return newTile.transform.position;
+		Tiles.Add(new Point(x, y), newTile);
 	}
 
 	private string[] ReadLevelText()
