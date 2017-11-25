61a9de34aa8909e7ede278a67fe8c3fb7a298ec5
DeMontrius Simon <DeMontriusSimon@gmail.com>
2017-11-24 20:08:52 -0500

    5.4 Hover Icon
    Created a hover icon to let the player know what tower they are holding after they click the button.

--- Tower Defense Tutorial/Assets/Scripts/TowerBtn.cs
+++ Tower Defense Tutorial/Assets/Scripts/TowerBtn.cs
@@ -7,11 +7,22 @@ public class TowerBtn : MonoBehaviour
 	[SerializeField]
 	private GameObject towerPrefab;
 
+	[SerializeField]
+	private Sprite sprite;
+
 	public GameObject TowerPrefab
 	{
 		get
 		{
 			return towerPrefab;
+		}
+	}
+
+	public Sprite Sprite
+	{
+		get
+		{
+			return sprite;
 		}
 	}
 }
