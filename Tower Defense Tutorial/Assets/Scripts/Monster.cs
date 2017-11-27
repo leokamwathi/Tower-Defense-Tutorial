744ecc5c13a4eec43c804419465504b74e679ff6
DeMontrius Simon <DeMontriusSimon@gmail.com>
2017-11-26 22:28:17 -0500

    7.2 Spawn Effect
    Add a spawn effect that makes the monster look as if it is actually going out of the portal.

--- Tower Defense Tutorial/Assets/Scripts/Monster.cs
+++ Tower Defense Tutorial/Assets/Scripts/Monster.cs
@@ -7,5 +7,22 @@ public class Monster : MonoBehaviour
 	public void Spawn()
 	{
 		transform.position = LevelManager.Instance.BluePortal.transform.position;
+
+		StartCoroutine(Scale(new Vector3(0.1f, 0.1f), new Vector3(1, 1)));
+	}
+
+	public IEnumerator Scale(Vector3 from, Vector3 to)
+	{
+		float progress = 0;
+
+		while (progress <= 1)
+		{
+			transform.localScale = Vector3.Lerp(from, to, progress);
+
+			progress += Time.deltaTime;
+
+			yield return null;
+		}
+		transform.localScale = to;
 	}
 }
