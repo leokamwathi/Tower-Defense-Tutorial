3eab86a81b9c25e3068a0f2a37ca3f12d93eb50d
DeMontrius Simon <DeMontriusSimon@gmail.com>
2017-11-27 22:14:26 -0500

    7.4 Animating Monster (Red)
    The animation for the Red Monster is added.

--- Tower Defense Tutorial/Assets/Scripts/GameManager.cs
+++ Tower Defense Tutorial/Assets/Scripts/GameManager.cs
@@ -36,7 +36,7 @@ public class GameManager : Singleton<GameManager>
 	// Use this for initialization
 	void Start ()
 	{
-		Currency = 100;
+		Currency = 1000;
 	}
 	
 	// Update is called once per frame
@@ -80,7 +80,7 @@ public class GameManager : Singleton<GameManager>
 	{ 
 		LevelManager.Instance.GeneratePath();
 
-		int monsterIndex = Random.Range(0, 4);
+		int monsterIndex = 1; //Random.Range(0, 4);
 		string type = string.Empty;
 		switch (monsterIndex)
 		{
--- Tower Defense Tutorial/Assets/Scripts/Monster.cs
+++ Tower Defense Tutorial/Assets/Scripts/Monster.cs
@@ -15,6 +15,8 @@ public class Monster : MonoBehaviour
 
 	public bool IsActive { get; set; }
 
+	private Animator myAnimator;
+
 	private void Update()
 	{
 		Move();
@@ -23,6 +25,8 @@ public class Monster : MonoBehaviour
 	public void Spawn()
 	{
 		transform.position = LevelManager.Instance.BluePortal.transform.position;
+
+		myAnimator = GetComponent<Animator>();
 
 		StartCoroutine(Scale(new Vector3(0.1f, 0.1f), new Vector3(1, 1)));
 
@@ -57,6 +61,7 @@ public class Monster : MonoBehaviour
 			{
 				if (path != null && path.Count > 0)
 				{
+					Animate(GridPosition, path.Peek().GridPosition);
 					GridPosition = path.Peek().GridPosition;
 					destination = path.Pop().WorldPosition;
 				}
@@ -70,7 +75,39 @@ public class Monster : MonoBehaviour
 		{
 			this.path = newPath;
 			GridPosition = path.Peek().GridPosition;
+			Animate(GridPosition, path.Peek().GridPosition);
 			destination = path.Pop().WorldPosition;
+		}
+	}
+
+	private void Animate(Point currentPos, Point newPos)
+	{
+		if (currentPos.Y > newPos.Y)
+		{
+			//Moving Down
+			myAnimator.SetInteger("Horizontal", 0);
+			myAnimator.SetInteger("Vertical", 1);
+		}
+		else if(currentPos.Y <newPos.Y)
+		{
+			//Moving Up
+			myAnimator.SetInteger("Horizontal", 0);
+			myAnimator.SetInteger("Vertical", -1);
+		}
+		if (currentPos.Y == newPos.Y)
+		{
+			if (currentPos.X > newPos.X)
+			{
+				//Move to the left
+				myAnimator.SetInteger("Horizontal", -1);
+				myAnimator.SetInteger("Vertical", 0);
+			}
+			else if (currentPos.X < newPos.X)
+			{
+				myAnimator.SetInteger("Horizontal", 1);
+				myAnimator.SetInteger("Vertical", 0);
+				//Moving to the right
+			}
 		}
 	}
 }
